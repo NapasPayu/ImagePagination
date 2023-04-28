c50e7e95e498f07d388769dd20f7147ca439756f
NapasPayu <napas.psonthi@hotmail.com>
2023-04-29 00:58:35 +0700

    create app and main activity

---
+++ app/src/main/java/com/napas/imagepagination/ImagePaginationApp.kt
@@ -0,0 +1,7 @@
+package com.napas.imagepagination
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ImagePaginationApp : Application()
---
+++ app/src/main/java/com/napas/imagepagination/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.napas.imagepagination
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp() = navController.navigateUp()
+}
